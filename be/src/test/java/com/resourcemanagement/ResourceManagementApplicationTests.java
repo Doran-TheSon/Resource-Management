792731cf0dec948b7e0ddc9d906157ef52da6ca5
Senior Son The <buitheson2004@gmail.com>
2026-07-15 13:46:22 +0700

    feature(be): Spring Boot skeleton with HealthController, CORS config, H2 dev profile, Docker multi-stage build
    - be/: Spring Boot skeleton with HealthController, CORS config, H2 dev profile, Docker multi-stage build

---
+++ be/src/test/java/com/resourcemanagement/ResourceManagementApplicationTests.java
@@ -0,0 +1,12 @@
+package com.resourcemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
